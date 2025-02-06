43a1a3f8a48cd525b9227daee2dc263a74a0f369
Gail Tao Yi <xitianyun@foxmail.com>
2025-02-07 01:20:11 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '5096459e-08f1-4157-9f63-26df26e448f5';
 
-let proxyIP = '';
+let proxyIP = 'proxyip.hk.fxxk.dedyn.io';
 
 
 if (!isValidUUID(userID)) {
